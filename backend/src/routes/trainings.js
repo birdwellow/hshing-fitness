c534b2c9fe30d54e8aad56cf47ffd0962991b924
Florian Vogel <florian.vogel@iteratec.de>
2018-12-01 12:54:08 +0100

    Implement full basic ReST API

--- backend/src/routes/trainings.js
+++ backend/src/routes/trainings.js
@@ -3,117 +3,68 @@ var router = express.Router();
 var Training = require("../model/Training");
 
 /* Create */
-// router.post('/', function(req, res){
+router.post("/", function(req, res) {
+  var training = new Training(req.body);
 
-//     var newFlight = new Flight(req.body);
-
-//     newFlight.save(function(err, savedFlight) {
-//         if (err) {
-//             res.status(400).send(err);
-//         } else {
-//             res.status(200).send(savedFlight);
-//         }
-//     });
-// });
+  training.save(function(err, resultTraining) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.status(200).send(resultTraining);
+    }
+  });
+});
 
 /* Read */
 router.get("/", function(req, res) {
   var queryArg = {};
 
-  // if (req.query.maxprice) {
-  //     queryArg.price = {$lte: req.query.maxprice};
-  // }
-  // if (req.query.seats) {
-  //     queryArg.seats = {$gte: req.query.seats};
-  // }
-  // if (req.query.from) {
-  //     queryArg.departureCity = req.query.from;
-  // }
-  // if (req.query.to) {
-  //     queryArg.arrivalCity = req.query.to;
-  // }
   // if (req.query.date) {
   //     var date = new Date(req.query.date);
   //     queryArg.date = date;
   // }
 
   Training.find(queryArg, function(err, trainings) {
-    var status = 204;
-    if (trainings && trainings.length > 0) {
-      status = 200;
-    }
-
-    res.status(status).send(trainings);
+    res.status(200).send(trainings);
   });
 });
 
-// router.get('/:id', function(req, res){
-//     Flight.findById(req.params.id, function (err, flight) {
-//         var status = 404;
-//         if (flight) {
-//             status = 200;
-//         }
-//         res.status(status).send(flight);
-//     });
-// });
+router.get("/:id", function(req, res) {
+  Training.findById(req.params.id, function(err, training) {
+    var status = 404;
+    if (training) {
+      status = 200;
+    }
+    res.status(status).send(training);
+  });
+});
 
 /* Update */
-// router.put('/', function(req, res){
+router.put("/", function(req, res) {
+  var updateTraining = req.body;
 
-//     var updateFlight = req.body;
+  Training.update(
+    { _id: updateTraining._id },
+    { $set: updateTraining },
+    function(err) {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(204).send();
+      }
+    }
+  );
+});
 
-//     Flight.update({ _id: updateFlight._id }, { $set: updateFlight}, function(err) {
-//         if (err) {
-//             res.status(400).send(err);
-//         } else {
-//             res.status(204).send();
-//         }
-//     });
-// });
-
-/* Update with Booking */
-// router.put('/:id/bookings', function(req, res){
-
-//     var flightId = req.params.id;
-//     var seats = req.query.seats;
-//     if (!flightId || !seats) {
-//         res.status(400).send("No flightId or seats specified");
-//         return;
-//     }
-
-//     Flight.findById(flightId, function (err, flight) {
-//         if (!flight || flight.seats - seats < 0) {
-//             res.status(404).send("Flight not found or no seats available");
-//         }
-
-//         flight.seats = flight.seats - seats;
-
-//         if (flight.seats <= 0) {
-//             Flight.remove({_id: flight._id}, function (err) {
-//                 res.status(200).send();
-//             });
-//             return;
-//         }
-
-//         flight.save(function(err) {
-//             if (!err) {
-//                 res.status(200).send();
-//             }
-//         });
-
-//     });
-
-// });
-
-// /* Delete */
-// router.delete('/:id', function(req, res){
-//     console.log("Removing " + req.params.id);
-//     Flight.remove({_id: req.params.id}, function (err) {
-//         if (err) {
-//             res.status(400).send(err);
-//         }
-//         res.status(204).send();
-//     });
-// });
+/* Delete */
+router.delete("/:id", function(req, res) {
+  console.log("Removing " + req.params.id);
+  Training.remove({ _id: req.params.id }, function(err) {
+    if (err) {
+      res.status(400).send(err);
+    }
+    res.status(204).send();
+  });
+});
 
 module.exports = router;
